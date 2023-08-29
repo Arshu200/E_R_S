92cfb18c379cdb233255faf91286a2ca17012aa0
[Arshu200] <[shaik22arshad@gmail.com]>
2023-08-29 08:43:55 +0530

    changes to  Commit

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,7 +18,7 @@ module.exports.createUser = async function (req, res) {
         req.user=await User.create({
         name: req.body.name,
         email: req.body.email,
-        isAdmin: true,
+        isAdmin: false,
         password: req.body.password,
       });
       console.log("User created successfully");
